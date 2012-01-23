0face448eee8702e61ec374e1b8ebc2e5e1d2be4
Mike Glenn <mglenn@ilude.com>
2012-01-22 22:19:09 -0500

    forgot to save a file

--- spec/box_spec.rb
+++ spec/box_spec.rb
@@ -3,9 +3,9 @@ require 'vagabond'
 describe "Vagabond::Box" do
   describe ".new" do
     it "should have a name" do
-    box = Vagabond::Box.new('rspec_test')
-    box.name.should == 'rspec_test'
-  end
+      box = Vagabond::Box.new('rspec_test')
+      box.name.should == 'rspec_test'
+    end
   end
  
   
